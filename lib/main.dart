d80f7b654754a8ea91ee4cfea1393ec78ff72a82
Jiaxen <jiaxen.lau2@gmail.com>
2021-08-28 14:31:18 +0100

    Created xylophone buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -9,17 +10,36 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: Container(
-                child: TextButton(
-                    onPressed: () {
-                      final player = AudioCache();
-                      player.play('note2.wav');
-                    },
-                    child: Text('play sound'))),
+          child: Column(
+            children: [
+              xylophoneButton(filename: 'note1.wav', color: Colors.red),
+              xylophoneButton(filename: 'note2.wav', color: Colors.orange),
+              xylophoneButton(filename: 'note3.wav', color: Colors.yellow),
+              xylophoneButton(filename: 'note4.wav', color: Colors.green),
+              xylophoneButton(filename: 'note5.wav', color: Colors.blue),
+              xylophoneButton(filename: 'note6.wav', color: Colors.indigo),
+              xylophoneButton(filename: 'note7.wav', color: Colors.purple),
+            ],
           ),
         ),
       ),
     );
   }
+
+  Expanded xylophoneButton({String filename, Color color}) => Expanded(
+        child: TextButton(
+          onPressed: () {
+            final player = AudioCache();
+            player.play(filename);
+          },
+          child: Container(
+            color: color,
+          ),
+          style: ButtonStyle(
+            padding: MaterialStateProperty.all<EdgeInsets>(
+              EdgeInsets.all(0),
+            ),
+          ),
+        ),
+      );
 }
